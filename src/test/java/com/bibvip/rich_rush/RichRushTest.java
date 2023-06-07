1a35967bf971d13c051be57d5802c771a0786183
ef-ola <paul>
2023-06-07 15:58:14 +0800

    Adding Test Folder -1U Fiesta -Rich Rush

--- src/test/java/com/bibvip/rich_rush/RichRushTest.java
+++ src/test/java/com/bibvip/rich_rush/RichRushTest.java
@@ -4,10 +4,15 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+
 import static com.bibvip.config.DriverConfig.getChromeConfig;
 import static com.bibvip.config.DriverConfig.getDriverWait;
+import static com.bibvip.util.AppUtil.getElementWithPolling;
 
 /**
  * @author ef-Olaf
@@ -23,6 +28,14 @@ public class RichRushTest {
 
     private final static String RICH_RUSH_BTN = "//*[@id=\"__layout\"]/span/div/div[1]/header/div[1]/div[3]/span";
 
+    @Test
+    public void setup() {
+        chromeConfig = getChromeConfig();
+        WebDriverWait webDriverWait = getDriverWait(chromeConfig);
+
+        WebElement close_ads = getElementWithPolling(webDriverWait, By.ByXPath.xpath(ADS));
+        close_ads.click();
+    }
 
 
 }
